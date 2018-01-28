eea6ee840fd024987a4bc6b79846020e875589b4
Seong ho Hong <grohong76@gmail.com>
2018-01-28 17:06:20 +0900

    제곱수의 합

---
+++ Basics/다이나믹/문제풀이/src/main/java/타일채우기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by seonghohong on 2018. 1. 27..
+ */
+public class 타일채우기 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        
+        int[] cach = new int[input];
+    }
+}
