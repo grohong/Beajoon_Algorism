baffdbd7f2cf5002fe5e0139d92d39560775b872
Seong ho Hong <grohong76@gmail.com>
2018-03-02 18:37:06 +0900

    암호문제 풀이

--- Basics/다이나믹/문제풀이/src/main/java/암호코드.java
+++ Basics/다이나믹/문제풀이/src/main/java/암호코드.java
@@ -8,98 +8,44 @@ public class 암호코드 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        char[] inputString = input.toCharArray();
 
-        int[] cach = new int[inputString.length+1];
-        cach[0] = 1;
-        cach[1] = 1;
-        for (int i=2; i<=inputString.length; i++) {
-            String temp = ""+inputString[i-2]+""+inputString[i-1]+"";
+        int[] cach = new int[input.length()];
 
-            int tempInt = Integer.parseInt(temp);
-            if (tempInt == 0) {
-                cach[i] = 0;
-                continue;
-            }
+        int startInt = Integer.parseInt(""+input.charAt(0)+""+""+input.charAt(1)+"");
 
-            if (tempInt == 30 || tempInt == 40 || tempInt == 50 || tempInt == 60 || tempInt == 70 || tempInt == 80 || tempInt == 90) {
-                cach[i] = 0;
-                continue;
-            }
-
-            if (1 <= tempInt && tempInt < 10) {
-                cach[i] = cach[i-1];
-            } else if (tempInt == 10) {
-                cach[i] = cach[i-1] + 1;
-            } else if (11 <= tempInt && tempInt <= 26) {
-                cach[i] = (cach[i-1] + cach[i-2])%1000000;
-            } else {
-                cach[i] = cach[i-1];
-            }
-        }
-
-        if (inputString[0] == '0') {
-            System.out.println("0");
-        } else if (inputString.length == 1) {
-            System.out.println("1");
-        } else if (inputString.length <= 2 && 27 <= Integer.parseInt(input) && Integer.parseInt(input) <= 99){
-            System.out.println("1");
+        if (startInt == 10 && startInt == 20) {
+            cach[0] = 1;
+            cach[1] = 1;
+        } else if (startInt <= 26 && startInt != 10 && startInt != 20) {
+            cach[0] = 1;
+            cach[1] = 2;
+        } else if (startInt > 26 && startInt % 10 != 0) {
+            cach[0] = 1;
+            cach[1] = 1;
         } else {
-            System.out.println(cach[inputString.length]);
+            cach[0] = 0;
+            cach[0] = 0;
         }
 
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine().trim();
-        int n = s.length();
-        s = " " + s;
-        int[] d = new int[n+1];
-        d[0] = 1;
-        for (int i=1; i<=n; i++) {
-            int x = s.charAt(i) - '0';
-            if (1 <= x && x <= 9) {
-                d[i] += d[i-1];
-                d[i] %= mod;
+
+        for (int i=2; i<input.length(); i++) {
+            int cachInt = Integer.parseInt(""+input.charAt(i-1)+""+""+input.charAt(i)+"");
+            System.out.println("---------------------");
+            System.out.println(cachInt);
+
+            if (cachInt <= 26 && cachInt % 10 != 0) { cach[i] = (cach[i-1] + cach[i-2]) % mod; }
+            else if (cachInt <= 26 && cachInt % 10 == 0) { cach[i] = (cach[i-1]) % mod; }
+            else if (cachInt > 26 && cachInt % 10 != 0) { cach[i] = (cach[i-1]) % mod; }
+            else {
+                cach[i] = 0;
+                break;
             }
-            if (i==1) {
-                continue;
-            }
-            if (s.charAt(i-1) == '0') {
-                continue;
-            }
-            x = (s.charAt(i-1)-'0')*10 + (s.charAt(i)-'0');
-            if (10 <= x && x <= 26) {
-                d[i] += d[i-2];
-                d[i] %= mod;
-            }
+
+            System.out.println("log");
+            System.out.println(cach[i]);
+            System.out.println("---------------------");
         }
-        System.out.println(d[n]);
+
+        System.out.println(cach[input.length()-1]);
     }
-
-//    public static void main(String args[]) {
-//        Scanner sc = new Scanner(System.in);
-//        String s = sc.nextLine().trim();
-//        int n = s.length();
-//        s = " " + s;
-//        int[] d = new int[n+1];
-//        d[0] = 1;
-//        for (int i=1; i<=n; i++) {
-//            int x = s.charAt(i) - '0';
-//            if (1 <= x && x <= 9) {
-//                d[i] += d[i-1];
-//                d[i] %= mod;
-//            }
-//            if (i==1) {
-//                continue;
-//            }
-//            if (s.charAt(i-1) == '0') {
-//                continue;
-//            }
-//            x = (s.charAt(i-1)-'0')*10 + (s.charAt(i)-'0');
-//            if (10 <= x && x <= 26) {
-//                d[i] += d[i-2];
-//                d[i] %= mod;
-//            }
-//        }
-//        System.out.println(d[n]);
-//    }
 }
