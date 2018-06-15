f35e609069d5bfdae5e9281e82b3acfc8b8fad87
Seong ho Hong <grohong76@gmail.com>
2018-06-15 23:33:16 +0900

    python 풀이

---
+++ Graph/DFS BFS(1260)/src/main/java/test.java
@@ -0,0 +1,4 @@
+public interface test {
+    void testIngerface();
+    int test = 0;
+}
---
+++ python3 풀이/진법 변환2/main.py
@@ -0,0 +1,13 @@
+class solution:
+    def __init__(self, method):
+        self.method = method
+
+    def transfer(self, number):
+
+        
+
+
+input_number_list = [int(x) for x in input().split()]
+
+input_number = input_number_list[0]
+method = input_number_list[1]
