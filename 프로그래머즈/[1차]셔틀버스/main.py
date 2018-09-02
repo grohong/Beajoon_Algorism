3bc1c2abe2257b6ea62181ec02dfe6c35971dc95
Seong ho Hong <grohong76@gmail.com>
2018-09-02 13:25:08 +0900

    [1차]셔틀버스

---
+++ 프로그래머즈/[1차]셔틀버스/main.py
@@ -0,0 +1,114 @@
+from collections import deque
+
+# 셔틀은 09:00부터 총 n회 t분 간격으로 역에 도착하며, 하나의 셔틀에는 최대 m명의 승객이 탈 수 있다.
+class Time:
+
+    def __init__(self, time):
+        times = time.split(":")
+        self.hour = int(times[0])
+        self.min = int(times[1])
+
+    def add_time(self, time):
+        self.min += time
+
+        if int(self.min/60) != 0:
+            self.hour += int(self.min/60)
+            self.min = int(self.min % 60)
+
+        if self.min == -1:
+            self.min = 59
+            self.hour -= 1
+
+    def __str__(self):
+        return "%s:%s" % (str(self.hour).zfill(2), str(self.min).zfill(2))
+
+    def __repr__(self):
+        return "%s:%s" % (str(self.hour).zfill(2), str(self.min).zfill(2))
+
+    def __lt__(self, other):
+        if self.hour == other.hour:
+            return self.min < other.min
+        else:
+            return self.hour < other.hour
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+
+def solution(n, t, m, timetable):
+    answer = ''
+
+    bus_num = n
+    bus_term = t
+    bus_person = m
+
+    start_bus_time = Time("09:00")
+
+    times = []
+    for time in timetable:
+        times.append(Time(time))
+
+    times = sorted(times)
+
+    if len(times) == 0:
+        for x in range(bus_num):
+            start_bus_time.add_time(bus_term)
+
+        answer = str(start_bus_time)
+        return answer
+
+
+    tmp_time = Time("00:00")
+    full_flag = False
+    tmp_person = 0
+    idx = 0
+    while(True):
+        if len(times) == idx:
+            break
+
+        time = times[idx]
+
+        if bus_num == 1:
+            if full_flag == False and (start_bus_time > time or start_bus_time == time):
+                tmp_person += 1
+                tmp_time = time
+                idx += 1
+
+                if bus_person == tmp_person:
+                    full_flag = True
+                    break
+            else:
+                break
+
+        else:
+            if start_bus_time > time:
+                if tmp_person != bus_person:
+                    tmp_person += 1
+                    idx += 1
+                else:
+                    start_bus_time.add_time(bus_term)
+                    tmp_person = 0
+                    bus_num -= 1
+            else:
+                start_bus_time.add_time(bus_term)
+                bus_num -= 1
+                tmp_person = 0
+
+
+
+
+    if full_flag == False:
+        answer = str(start_bus_time)
+    elif full_flag == True:
+        tmp_time.add_time(-1)
+        answer = str(tmp_time)
+
+    return answer
+
+
+# print(solution(1, 1, 5, ["08:00", "08:01", "08:02", "08:03", "08:58"]))
+# print(solution(2, 60, 2, ["08:00", "08:13", "08:14", "10:00"]))
+print(solution(2, 60, 2, []))
+# print(solution(1, 1, 5, ["00:01", "00:01", "00:01", "00:01", "00:01"]))
+# print(solution(1, 1, 1, ["23:59"]))
+# print(solution(10, 60, 46, ["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"]))
